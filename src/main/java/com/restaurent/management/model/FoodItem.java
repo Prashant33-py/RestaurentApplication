f1fc1ad13579f59bf06c09a642e334709e45b702
Prashant33-py <pvasala786@gmail.com>
2025-03-25 14:17:52 +0530

    Initial commit

---
+++ src/main/java/com/restaurent/management/model/FoodItem.java
@@ -0,0 +1,13 @@
+package com.restaurent.management.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FoodItem {
+
+
+}
